af1709d74e9665aaa39e63a67f7a37e6247f61f8
dietrichen <ed819118@wcupa.edu>
2016-10-02 15:26:17 -0400

    3:26 10022016

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,23 +3,59 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-	public float jumpForce = 6f;
+	public float jumpForce = 10f;
 	private Rigidbody2D rigidBody;
+	public Animator animator;
+	public float runningSpeed = 1.5f;
 
-	void Awake() {
+	void Start()
+	{
+		animator.SetBool("isAlive", true);
+	}
+
+	void Awake()
+	{
 		rigidBody = GetComponent<Rigidbody2D>();
 
+	}
+
+	void Jump()
+	{
+		if (IsGrounded())
+		{
+			rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+		}
 	}
 
 	void Update()
 	{
 		if (Input.GetMouseButtonDown(0))
-			Jump();
-		}
+		{
+			Jump();}
+		animator.SetBool("isGrounded", IsGrounded());
+	}
 
-	void Jump() 
+	void FixedUpdate()
 	{
-		rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+		if (rigidBody.velocity.x < runningSpeed)
+		{
+			rigidBody.velocity = new Vector2(runningSpeed, rigidBody.velocity.y);
+		}
+	}
+
+
+	public LayerMask groundLayer;
+
+	bool IsGrounded()
+	{
+		if (Physics2D.Raycast(this.transform.position, Vector2.down, 0.2f, groundLayer.value))
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
 	}
 
 
