ccd02c1562492aeda31cf31d2fcc96f1e2e6b08b
dietrichen <ed819118@wcupa.edu>
2016-10-02 11:42:48 -0400

    Added Assets
    Added assets for player and floor

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float jumpForce = 6f;
+	private Rigidbody2D rigidBody;
+
+	void Awake() {
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			Debug.Log("Left mouse butten clicked");
+		}
+	}
+
+		void Jump(){
+		rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+		
+		}
+	
+	}
+
