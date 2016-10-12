49e1191b1ee10b53ec8229981404a48b0536472f
dietrichen <ed819118@wcupa.edu>
2016-10-11 22:47:10 -0400

    git works now....
    ....

---
+++ Assets/Scripts/FinishLineTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishLineTrigger : MonoBehaviour
+{
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Player") {
+			Debug.Log ("FinishLine");
+			PlayerController.instance.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+			PlayerController.instance.Winner ();
+			LevelGenerator.instance.RemoveLevel ();
+		}
+	}
+}
+
